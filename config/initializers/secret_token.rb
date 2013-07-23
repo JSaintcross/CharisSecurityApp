97ecbb193f0038c4c6ecec0fd5ad8f65fd4948bf
Nathan Norwood <nathan.norwood@charisservices.com>
2013-07-23 10:02:20 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecurityApp::Application.config.secret_token = '685dd62b7d89cf3287d3ecb3d2316025b43996833b5ae4dbb471ba0cd34a91e8fdd1b1f0cb4e0708dfe7f87c2fabea855f63c2c3c4d920c27aabb93d6636216f'
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+#config/routes.rb
+
+resources :user_sessions 
+  match 'login' => "user_sessions#new",      :as => :login
+  match 'logout' => "user_sessions#destroy", :as => :logout
+
+  resources :users  # give us our some normal resource routes for users
+  resources :user, :as => 'account'  # a convenience route
+
+  match 'signup' => 'users#new', :as => :signup
+
+  root :to => 'users#new'
