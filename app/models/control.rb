61712c93c4dbfb0d606a25bebeb820ebd85f55a0
Jake Saintcross <jacob.saintcross@charisservices.com>
2013-07-31 11:45:42 -0400

    heirarchy established

--- app/models/control.rb
+++ app/models/control.rb
@@ -1,2 +1,6 @@
 class Control < ActiveRecord::Base
+belongs_to :sources
+has_many :subcontrols
+belongs_to :control_families
+belongs_to :baselines
 end
--- app/models/subcontrol.rb
+++ app/models/subcontrol.rb
@@ -1,2 +1,4 @@
 class Subcontrol < ActiveRecord::Base
+  belongs_to :controls
+  belongs_to :baselines
 end
